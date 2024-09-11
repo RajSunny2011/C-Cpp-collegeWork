7a508c4c7f536b5a1923a567075228b077e98ecd
RajSunny2011 <rajsatvik004@gmail.com>
2024-09-11 12:01:47 +0530

    Cleaned up

--- CProgramming/DSA/class.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    int RollNo[3]={1,2,3};
-    char name[3][20]={"Sunny Raj","Satvik","Aman"};
-    for (int i=0;i<3;i++){
-        printf("%s",name[i]);
-        printf("\n");
-    }
-}
