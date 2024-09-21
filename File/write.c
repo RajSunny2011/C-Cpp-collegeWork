ffb357aac6d5f40c5b5072036be935f50bc50a00
Sunny <83127107+RajSunny2011@users.noreply.github.com>
2024-09-21 18:49:57 +0530

    removed duplicate files

--- File/write.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-    FILE* fptr;
-    fptr = fopen("file.txt", "w");
-    fprintf(fptr, "Hello");
-    fclose(fptr);
-    return 0;
-}
--- Tests/float.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    float x = 0.1, y = 0.2, z;
-    z = x + y;
-    printf("Result = %.1f", z);
-
-    return 0;
-}
