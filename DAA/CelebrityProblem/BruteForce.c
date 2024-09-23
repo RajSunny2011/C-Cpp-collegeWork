c32bcd3dbc245a900fed74bd595b1e57cf47aca5
RajSunny2011 <rajsatvik004@gmail.com>
2024-09-23 13:27:14 +0530

    new file:   DAA/CelebrityProblem/BruteForce.c

---
+++ DAA/CelebrityProblem/BruteForce.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int **createMat(int size){
+    int **mat = (int**)malloc(size * sizeof(int*));
+    for (int i = 0; i < size; i++) {
+        mat[i] = (int*)malloc(size * sizeof(int));
+    }
+    return mat;
+}
+
+int findCelebrity(int** Mat, int n){
+    int celebrtiy = -1;
+    for (int i = 0; i<n; i++){
+        bool knows_anyone = false;
+        bool everyone_knows = true;
+        for (int j = 0; j<n; j++){
+            if (Mat[i][j]){
+                knows_anyone = true;
+                break;
+            }
+        }
+        if (knows_anyone){
+            continue;
+        }
+        for (int j = 0; j < n; j++){
+            if(i!=j && !(Mat[j][i])){
+                everyone_knows = false;
+                break;
+            }
+        }
+        if (!(everyone_knows)){
+            continue;
+        }
+        if (!(knows_anyone) && everyone_knows){
+            celebrtiy = i;
+            break;
+        }
+    }
+    return celebrtiy;
+}
+
+int main(){
+    int size;
+    printf("Enter matrix size: ");
+    scanf("%d", &size);
+    int **mat = createMat(size);
+    printf("Enter elements of matrix A:\n");
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            scanf("%d", &mat[i][j]);
+        }
+    }
+    printf("%d\n",findCelebrity(mat, size));
+    return 0;
+}
